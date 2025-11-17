63c5fa003482a29e08c28feaeb7a6ec839d7af69
Zubaida <zubaidasuleiman89@gmail.com>
2025-11-17 12:37:50 +0100

    Create pattern_drawing.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+size = int(input("Enter the size of the pattern: "))
+
+row = 0
+
+# Use a while loop for each row
+while row < size:
+    # Use a for loop to print asterisks
+    for _ in range(size):
+        print("*", end="")
+    print()  # Move to next line after each row
+    row += 1
+    
